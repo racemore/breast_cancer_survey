8a6cf6a99904a1fc9a4b609c0a06f8aae8882976
racemore <aracelymoreno101@gmail.com>
2022-04-12 02:00:58 -0400

    clean code, change var names

--- gami_sms.py
+++ gami_sms.py
@@ -1,19 +1,7 @@
-
-
-import os
 import json
 from twilio.rest import Client
 from decouple import config
-import requests
-from json import JSONEncoder
 
-class QueryEncoder(JSONEncoder):
-        def default(self, o):
-            return o.__dict__
-
-# ACCOUNT_SID = os.getenv('ACCOUNT_SID')
-# AUTH_TOKEN = os.getenv('AUTH_TOKEN')
-# ASSISTANT_ID = os.getenv('ASSISTANT_ID')
 ACCOUNT_SID = config('ACCOUNT_SID')
 AUTH_TOKEN = config('AUTH_TOKEN')
 ASSISTANT_ID = config('ASSISTANT_ID')
@@ -24,14 +12,14 @@ queries = client.autopilot \
                 .queries \
                 .list(limit=100)
 
-lst = []
+users = []
 for record in queries:
     sender_id = record.dialogue_sid
-    msg_dct = {"sender_id":sender_id}
-    msg_dct |= record.results
-    lst.append(msg_dct)
+    user_data = {"sender_id":sender_id}
+    user_data |= record.results
+    users.append(user_data)
 
-json_object = json.dumps(lst)
+json_object = json.dumps(users)
 
 with open("all_queries.json", "w") as outfile:
     outfile.write(json_object)
